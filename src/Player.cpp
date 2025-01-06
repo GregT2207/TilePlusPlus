4eeb6a03c787bcbd86a0dbff00853b265e7c02c5
greg <gregory.tuck@hotmail.com>
2025-01-06 16:34:28 +0000

    Implement basic jump

--- src/Player.cpp
+++ src/Player.cpp
@@ -9,6 +9,9 @@ void Player::handleEvents(SDL_Event &event)
 {
     GameObject::handleEvents(event);
 
+    const int movementSpeed = 200;
+    const int jumpPower = 300;
+
     if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
     {
         bool keyDown = (event.type == SDL_KEYDOWN);
@@ -16,11 +19,16 @@ void Player::handleEvents(SDL_Event &event)
         switch (event.key.keysym.sym)
         {
         case SDLK_a:
-            velocity.x = keyDown ? -200 : -0;
+            velocity.x = keyDown ? -movementSpeed : -0;
             break;
         case SDLK_d:
-            velocity.x = keyDown ? 200 : 0;
+            velocity.x = keyDown ? movementSpeed : 0;
             break;
+        case SDLK_SPACE:
+            if (keyDown)
+            {
+                velocity.y = -jumpPower;
+            }
         }
     }
 }
