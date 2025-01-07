7e5a143bec6b72d107b7bb5e0593f0b18396b0b6
greg <gregory.tuck@hotmail.com>
2025-01-07 16:14:45 +0000

    Render object name

--- src/SpriteRenderer.cpp
+++ src/SpriteRenderer.cpp
@@ -1,3 +1,4 @@
+#include <SDL2/SDL_test.h>
 #include "SpriteRenderer.hpp"
 #include "GameObject.hpp"
 
@@ -7,7 +8,13 @@ SpriteRenderer::SpriteRenderer(GameObject *owner) : Component(owner) {};
 
 void SpriteRenderer::render(SDL_Renderer *renderer, Vector pos, Vector size)
 {
+    int left = static_cast<int>(pos.x - (size.x / 2));
+    int top = static_cast<int>(pos.y - (size.y / 2));
+
     SDL_SetRenderDrawColor(renderer, 255, 170, 169, 255);
-    SDL_Rect rect = {static_cast<int>(pos.x - (size.x / 2)), static_cast<int>(pos.y - (size.y / 2)), static_cast<int>(size.x), static_cast<int>(size.y)};
+    SDL_Rect rect = {left, top, static_cast<int>(size.x), static_cast<int>(size.y)};
     SDL_RenderFillRect(renderer, &rect);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDLTest_DrawString(renderer, left + 10, top - 20, owner->getName().c_str());
 }
