30295d749e883558de711793532c6fd718dd681e
greg <gregory.tuck@hotmail.com>
2025-03-06 12:58:46 +0000

    Abstract render params to struct and getter

--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -1,10 +1,21 @@
 #pragma once
 #include <SDL2/SDL.h>
 #include "Component.hpp"
+#include "Vector.hpp"
 
 class ResourceManager;
 class Camera;
 class Transform;
+struct RenderParams
+{
+    Vector pos;
+    Vector size;
+    Vector dir;
+    float left;
+    float right;
+    float top;
+    float bottom;
+};
 
 class Renderer : public Component
 {
@@ -12,8 +23,10 @@ public:
     Renderer(SDL_Renderer *renderer, ResourceManager *resourceManager, const std::string &textureFilePath);
 
     void render(SDL_Renderer *renderer);
+    RenderParams getRenderParams(Transform *transform);
     void renderMainSprite(Camera *camera, Transform *transform);
     void renderInventoryItem(Camera *camera, Transform *transform);
+    void renderHealthBar(Camera *camera, Transform *transform);
     void renderNameTag(Camera *camera, Transform *transform);
 
 protected:
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -23,21 +23,31 @@ void Renderer::render(SDL_Renderer *renderer)
     {
         renderMainSprite(camera, transform);
         renderInventoryItem(camera, transform);
+        renderHealthBar(camera, transform);
         renderNameTag(camera, transform);
     }
 }
 
-void Renderer::renderMainSprite(Camera *camera, Transform *transform)
+RenderParams Renderer::getRenderParams(Transform *transform)
 {
     Vector pos = transform->getPosition();
     Vector size = transform->getSize();
     Vector dir = transform->getDirection();
 
     float left = pos.x - (size.x / 2);
+    float right = pos.x + (size.x / 2);
     float top = pos.y - (size.y / 2);
+    float bottom = pos.y + (size.y / 2);
 
-    SDL_Rect dest = camera->worldRectToScreenRect({left, top, size.x, size.y});
-    SDL_RenderCopyEx(renderer, texture, nullptr, &dest, 0, nullptr, dir.x > 0 ? SDL_FLIP_NONE : SDL_FLIP_HORIZONTAL);
+    return {pos, size, dir, left, right, top, bottom};
+}
+
+void Renderer::renderMainSprite(Camera *camera, Transform *transform)
+{
+    RenderParams rp = getRenderParams(transform);
+
+    SDL_Rect dest = camera->worldRectToScreenRect({rp.left, rp.top, rp.size.x, rp.size.y});
+    SDL_RenderCopyEx(renderer, texture, nullptr, &dest, 0, nullptr, rp.dir.x > 0 ? SDL_FLIP_NONE : SDL_FLIP_HORIZONTAL);
 }
 
 void Renderer::renderInventoryItem(Camera *camera, Transform *transform)
@@ -50,24 +60,28 @@ void Renderer::renderInventoryItem(Camera *camera, Transform *transform)
     if (!item)
         return;
 
-    Vector pos = transform->getPosition();
-    Vector size = transform->getSize();
-    Vector dir = transform->getDirection();
+    RenderParams rp = getRenderParams(transform);
 
-    float textureLength = item->getTile() ? size.x * 0.35f : size.x * 0.75f;
+    float textureLength = item->getTile() ? rp.size.x * 0.35f : rp.size.x * 0.75f;
     float textureOffset = 0.1f;
-    SDL_Rect destRect = camera->worldRectToScreenRect({dir.x > 0 ? pos.x + (size.x * textureOffset) : pos.x - (size.x * textureOffset) - textureLength,
-                                                       pos.y - (textureLength / 2),
+    SDL_Rect destRect = camera->worldRectToScreenRect({rp.dir.x > 0 ? rp.pos.x + (rp.size.x * textureOffset) : rp.pos.x - (rp.size.x * textureOffset) - textureLength,
+                                                       rp.pos.y - (textureLength / 2),
                                                        textureLength,
                                                        textureLength});
-    SDL_RenderCopyEx(renderer, item->getTexture(), nullptr, &destRect, item->getTile() ? 40 * (dir.x > 0 ? 1 : -1) : 0, nullptr, dir.x > 0 ? SDL_FLIP_NONE : SDL_FLIP_HORIZONTAL);
+    SDL_RenderCopyEx(renderer, item->getTexture(), nullptr, &destRect, item->getTile() ? 40 * (rp.dir.x > 0 ? 1 : -1) : 0, nullptr, rp.dir.x > 0 ? SDL_FLIP_NONE : SDL_FLIP_HORIZONTAL);
+}
+
+void Renderer::renderHealthBar(Camera *camera, Transform *transform)
+{
 }
 
 void Renderer::renderNameTag(Camera *camera, Transform *transform)
 {
+    RenderParams rp = getRenderParams(transform);
+
     std::string label = owner->getName();
     const int approxCharWidth = 8;
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    Vector stringPoint = camera->worldPosToScreenPos({transform->getPosition().x - (label.size() * approxCharWidth / camera->getZoom() / 2), transform->getPosition().y - (transform->getSize().y / 2) - 20});
+    Vector stringPoint = camera->worldPosToScreenPos({rp.pos.x - (label.size() * approxCharWidth / camera->getZoom() / 2), rp.top - 20});
     SDLTest_DrawString(renderer, stringPoint.x, stringPoint.y, label.c_str());
 }
