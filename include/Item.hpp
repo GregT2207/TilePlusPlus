b80a7e4eaf26ea7af8d24737a5532ae3a827214a
greg <gregory.tuck@hotmail.com>
2025-03-07 17:02:15 +0000

    Add missing qualifications

--- include/Item.hpp
+++ include/Item.hpp
@@ -4,14 +4,14 @@
 class Item
 {
 public:
-    Item(string name, SDL_Texture *texture, optional<Tile> tile = nullopt) : name(name), texture(texture), tile(tile) {};
+    Item(std::string name, SDL_Texture *texture, std::optional<Tile> tile = std::nullopt) : name(name), texture(texture), tile(tile) {};
 
     SDL_Texture *getTexture() const { return texture; }
-    optional<Tile> getTile() const { return tile; }
+    std::optional<Tile> getTile() const { return tile; }
 
-    string name;
+    std::string name;
 
 private:
     SDL_Texture *texture;
-    optional<Tile> tile;
+    std::optional<Tile> tile;
 };
