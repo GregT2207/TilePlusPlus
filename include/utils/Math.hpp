d65f828bc1ca0700d9ee01b7198d468bf332b6ca
greg <gregory.tuck@hotmail.com>
2025-03-11 18:18:36 +0000

    Improve lerp by clamping t and handling near-equal values

--- include/utils/Math.hpp
+++ include/utils/Math.hpp
@@ -5,6 +5,12 @@ namespace Math
 {
     inline float lerp(float a, float b, float t)
     {
+        t = std::min(t, 1.0f);
+        t = std::max(t, 0.0f);
+
+        if (fabs(a - b) < t)
+            return b;
+
         return a + (b - a) * t;
     }
 }
