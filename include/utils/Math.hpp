971f98bf9e26d1d63b91b6290308e0adde32bd89
greg <gregory.tuck@hotmail.com>
2025-03-11 18:18:50 +0000

    Tidy up

--- include/utils/Math.hpp
+++ include/utils/Math.hpp
@@ -5,8 +5,8 @@ namespace Math
 {
     inline float lerp(float a, float b, float t)
     {
-        t = std::min(t, 1.0f);
         t = std::max(t, 0.0f);
+        t = std::min(t, 1.0f);
 
         if (fabs(a - b) < t)
             return b;
