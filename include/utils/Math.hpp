f9e664c673c4d1c01902c94a64121e3ef3433703
greg <gregory.tuck@hotmail.com>
2025-03-11 17:58:02 +0000

    Fix circular dependency

--- include/utils/Math.hpp
+++ include/utils/Math.hpp
@@ -1,14 +1,9 @@
 #pragma once
-#include "../geometry/Vector.hpp"
+#include "geometry/Vector.hpp"
 
 namespace Math
 {
     inline float lerp(float a, float b, float t)
-    {
-        return a + (b - a) * t;
-    }
-
-    inline Vector lerp(Vector a, Vector b, float t)
     {
         return a + (b - a) * t;
     }
