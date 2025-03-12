bb16775a55dc4ec9f6f18e4ce7a1bacedc22e5e4
greg <gregory.tuck@hotmail.com>
2025-03-12 11:33:52 +0000

    Separate implementation from declaration

--- include/utils/Math.hpp
+++ include/utils/Math.hpp
@@ -4,14 +4,5 @@
 
 namespace Math
 {
-    inline float lerp(float a, float b, float t)
-    {
-        t = std::max(t, 0.0f);
-        t = std::min(t, 1.0f);
-
-        if (fabs(a - b) < t)
-            return b;
-
-        return a + (b - a) * t;
-    }
+    inline float lerp(float a, float b, float t);
 }
---
+++ src/utils/Math.cpp
@@ -0,0 +1,12 @@
+#include "Math.hpp"
+
+inline float Math::lerp(float a, float b, float t)
+{
+    t = std::max(t, 0.0f);
+    t = std::min(t, 1.0f);
+
+    if (fabs(a - b) < t)
+        return b;
+
+    return a + (b - a) * t;
+}
